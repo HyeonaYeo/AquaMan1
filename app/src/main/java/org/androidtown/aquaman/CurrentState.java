580de68e6700d9edf888f8c50b9b940c25a1a830
HyeonaYeo <yhy5225@naver.com>
2017-08-04 17:27:01 +0900

    Initial commit

---
+++ app/src/main/java/org/androidtown/aquaman/CurrentState.java
@@ -0,0 +1,23 @@
+package org.androidtown.aquaman;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ *
+ */
+
+public class CurrentState extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup rootview = (ViewGroup) inflater.inflate(R.layout.fragment_currentstate, container, false);
+
+        return rootview;
+    }
+}
